781e51c7d3d0d9c9b84c9fcd8551cbc02e1fd247
Derek Cruz <derekcruz184@yahoo.com>
2018-07-13 14:51:00 -0700

    7th update

---
+++ src/pages/method-learning/method-learning.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MethodLearningPage } from './method-learning';
+
+@NgModule({
+  declarations: [
+    MethodLearningPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(MethodLearningPage),
+  ],
+})
+export class MethodLearningPageModule {}
---
+++ src/pages/method-learning/method-learning.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the MethodLearningPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-method-learning',
+  templateUrl: 'method-learning.html',
+})
+export class MethodLearningPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MethodLearningPage');
+  }
+
+}
